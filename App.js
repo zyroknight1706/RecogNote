ee32c97d5d2a6b50ec2442a1c0a45d86502c7b12
zyroknight1706 <52271739+zyroknight1706@users.noreply.github.com>
2020-11-01 05:28:24 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,14 @@
+//main backend node server
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/Index.html");
+});
+
+app.listen(5000, function(){
+    console.log("server online on port 5000");
+});
