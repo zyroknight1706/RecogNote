dfe04494f0aac08ac065bda8a2427b116455435e
zyroknight1706 <52271739+zyroknight1706@users.noreply.github.com>
2020-11-01 08:48:48 +0530

    idk which number of commit

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 //main backend node server
-
+const webshot = require('node-webshot');
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
-app.use(express.static("Public"));
+app.use("/Public", express.static(__dirname+"/Public/"));
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/Index.html");
 });
@@ -12,3 +11,4 @@ app.get("/", function(req, res) {
 app.listen(5000, function(){
     console.log("server online on port 5000");
 });
+
--- Public/screen.js
+++ Public/screen.js
@@ -1,13 +1,2 @@
-const webshot = require("webshot");
 
-function takeshot() { 
-    // const webshot = require("webshot");
-    const optionselector = {
-        captureSelector: '#drawing-board'
-    };
-    webshot("#", "scr.png", optionselector, function(err){
-        if(!err){
-            console.log("it works");
-        } else { console.log(err) }
-    });
-}
+
