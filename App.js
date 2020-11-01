ab473b7664dc23be7c74aa629ee63563bd7a35ea
zyroknight1706 <52271739+zyroknight1706@users.noreply.github.com>
2020-11-01 18:09:32 +0530

    oh god this code is making me cry

--- App.js
+++ App.js
@@ -3,14 +3,51 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-var PdfPrinter = require('pdfmake');
-var printer = new PdfPrinter(fonts);
+// var PdfPrinter = require('pdfmake');
+// var printer = new PdfPrinter(fonts);
 var fs = require('fs');
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+const speechConfig = sdk.SpeechConfig.fromSubscription("88702d7d-b4e8-49a9-925a-c6a5298af91b", "eastus");
+const audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
+speechConfig.speechRecognitionLanguage = "en-IN";
+const recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+recognizer.recognizing = function(s,e){
+    console.log(`RECOGNIZING: Text=${e.result.text}`);
+};
+recognizer.recognized = function(s,e){
+    if (e.result.reason == ResultReason.RecognizedSpeech) {
+        console.log(`RECOGNIZED: Text=${e.result.text}`);
+    }
+    else if (e.result.reason == ResultReason.NoMatch) {
+        console.log("NOMATCH: Speech could not be recognized.");
+    }
+}
+
+recognizer.canceled = function(s, e){
+    console.log(`CANCELED: Reason=${e.reason}`);
+
+    if (e.reason == CancellationReason.Error) {
+        console.log(`"CANCELED: ErrorCode=${e.errorCode}`);
+        console.log(`"CANCELED: ErrorDetails=${e.errorDetails}`);
+        console.log("CANCELED: Did you update the subscription info?");
+    }
+
+    recognizer.stopContinuousRecognitionAsync();
+};
+
+recognizer.sessionStopped = function(s, e){
+    console.log("\n    Session stopped event.");
+    recognizer.stopContinuousRecognitionAsync();
+};
+
+
+
 app.use("/Public", express.static(__dirname+"/Public/"));
 // app.use(express.urlencoded({extended:true}));
 // app.use(express.json);
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/Index.html");
+    recognizer.startContinuousRecognitionAsync();
 });
 
 // app.post("/", function(req, res){
