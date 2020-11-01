ec9131d20089b2f89b16c1c2da5e0d71d595fdd3
zyroknight1706 <52271739+zyroknight1706@users.noreply.github.com>
2020-11-01 14:56:03 +0530

    which commit is this again?

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 //main backend node server
-const webshot = require('node-webshot');
+//const webshot = require('node-webshot');
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.use("/Public", express.static(__dirname+"/Public/"));
+// app.use(express.urlencoded({extended:true}));
+// app.use(express.json);
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/Index.html");
 });
+
+// app.post("/", function(req, res){
+//     // var canvas = document.querySelector("#drawing-board");
+
+//     // const url = canvas.toDataURL();
+//     // webshot(__dirname+"/index.html", "pic1.png", {siteType:"file", captureSelector: '#drawing-board'}, function(err){
+//     //     if(err) return;
+//     //     console.log("kheech li photo");
+//     // });
+// });
 
 app.listen(5000, function(){
     console.log("server online on port 5000");
