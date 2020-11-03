1cf0e22c8efc29a04cea60b49c58670ad319b987
zyroknight1706 <52271739+zyroknight1706@users.noreply.github.com>
2020-11-03 12:19:44 +0530

    here youu go

--- Public/screen.js
+++ Public/screen.js
@@ -3,22 +3,25 @@
 // var data = import("./data.js");
 const pdfDoc = require('pdfkit');
 var doc = new pdfDoc;
-
+var Speech = ['this works?'];
 doc.fontSize(10);
 
 function makepdf(res){
     res.type( "pdf" ).attachment('test.pdf');
     console.log("its working here");
-    // for(var i = 0; i < data.count; i++){
-    // doc.text(data.Speech[i],20,0);
-    // console.log("it should work till here");//lets see if it is wrorking at this point of time
-    // doc.image(data.ImageURLs[i],0,0,{width: 200, height: 100});
-    // }
+    doc.text(Speech[0], {
+        width: 412,
+        align: 'justify',
+        indent: 30,
+        columns: 2,
+        height: 300,
+        ellipsis: true
+      });
     doc.end();
     var buffers = [];
     doc.on("data", function(chunk){
         buffers.push(chunk);
-        console.log(chunk.toString());
+        // console.log(chunk.toString());
     });
     doc.on("end", function(){
         res.end(Buffer.concat(buffers), 'binary');
